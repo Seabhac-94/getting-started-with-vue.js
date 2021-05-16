b2ce18f922a79e669468214594670df77d1cb415
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-05-16 10:53:49 +0200

    app7 - introduction complete

---
+++ script.js
@@ -0,0 +1,70 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todo_list: [
+            { text: "Learn a bit of Vue.js" },
+            { text: "Update github repository" },
+            { text: "Learn more Vue.." }
+        ]
+    }
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js'
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: "",
+        placeholder: "test"
+    }
+});
+
+// app 7
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>',
+});
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Meat' }
+        ]
+    }
+});
+
